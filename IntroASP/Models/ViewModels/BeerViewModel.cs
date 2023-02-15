257ff3922a2dc789b16d6f868074cfa67cbae123
Javadeveloper7 <95065154+Javadeveloper7@users.noreply.github.com>
2023-02-15 13:26:27 -0500

    Agregar archivos de proyecto.

---
+++ IntroASP/Models/ViewModels/BeerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntroASP.Models.ViewModels
+{
+	public class BeerViewModel
+	{
+
+		[Required]
+		[Display(Name= "Nombre")]
+		public string Name { get; set; }
+
+		[Required]
+		[Display(Name= "Marca")]
+		public int BrandId { get; set; }
+	}
+}
